ff648cda489737c8bca86fa9f35bc9d63c93e61e
Aditya Bhagavatula <adityabp@hotmail.co.uk>
2025-08-04 13:53:03 -0500

    Changes

--- obj/Debug/net8.0/Insurance-Claims-Portal.AssemblyInfo.cs
+++ obj/Debug/net8.0/Insurance-Claims-Portal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Insurance-Claims-Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b5f631b05282910eacd60bfeb0063301b52cbf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Insurance-Claims-Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Insurance-Claims-Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
